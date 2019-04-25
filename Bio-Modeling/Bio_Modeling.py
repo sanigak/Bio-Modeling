c1153780e6a8c9de0ae6ba944ecba81b01873e01
Keith Saniga <sanigak@outlook.com>
2019-04-25 09:59:07 -0400

    Project for modeling population growth of biological systems

--- Bio-Modeling/Bio_Modeling.py
+++ Bio-Modeling/Bio_Modeling.py
@@ -1,1 +1,33 @@
+import numpy as np
 
+leafyGrowth = .04
+herbGrowth = .10
+predGrowth = .05
+
+leafyMat = np.full((10,10), 100)
+herbMat = np.full((10,10), 50)
+
+turn = 0 
+
+
+leafyPop = 100
+herbPop = 10
+predPop = 2
+
+
+while turn < 100:
+    turn+=1
+    leafyChange = (leafyPop * leafyGrowth) - herbPop
+    print("LEAFY: " + str(leafyChange))
+    if leafyPop > abs(leafyChange):
+        leafyPop = leafyPop + leafyChange
+        herbPop = (herbPop * herbGrowth) + herbPop
+    else:
+        leafyPop = 1
+        herbPop = herbPop+leafyChange
+    print()
+    print()
+    print("Turn: " + str(turn))
+    print("Leafs: " + str(leafyPop))
+    print("Herbs: " + str(herbPop))
+    
